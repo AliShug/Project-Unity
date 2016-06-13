16502b316731f3876d111dd8cd4ebcc7375dec97
Alistair <aw12700@my.bristol.ac.uk>
2016-06-13 14:28:54 +0100

    OVR Plugin
    Requires Oculus plugin on Unity 5.3-5.4

---
+++ Assets/VR_test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class VR_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OVRTracker tracker = OVRManager.tracker;
+
+		transform.position = tracker.GetPose().position;
+		transform.rotation = tracker.GetPose().orientation;
+	}
+}
