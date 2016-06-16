d8f4c06f3911c62b6e97600e1b85a62d9e11d972
Alistair <aw12700@my.bristol.ac.uk>
2016-06-16 15:51:25 +0100

    Added effects assets, working on comms

--- Assets/socket_test.cs
+++ Assets/socket_test.cs
@@ -1,29 +1,48 @@
 ﻿using UnityEngine;
 using System;
+using System.Net;
 using System.Text;
 using System.Collections;
 using System.Net.Sockets;
 
 public class socket_test : MonoBehaviour {
 
-	public int port = 14002;
+	public int targetPort = 14001;
+	public int bindPort = 14002;
 
-	private int n = 0;
-	private UdpClient sender;
-	private UdpClient receiver;
+	private Socket sockIn;
+	private Socket sockOut;
 
 	// Use this for initialization
-	void Start () {
-		sender = new UdpClient ("localhost", port);
-		receiver = new UdpClient (port + 1);
+	void Start() {
+		sockIn = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		sockIn.Bind(new IPEndPoint(IPAddress.Loopback, bindPort));
+
+		sockOut = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		sockOut.Connect(new IPEndPoint(IPAddress.Loopback, targetPort));
+
+//		// Stop a stupid error
+//		uint IOC_IN = 0x80000000;
+//		uint IOC_VENDOR = 0x18000000;
+//		uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
+//		udp.Client.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
+
+		Debug.Log("Bound " + bindPort.ToString());
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		string nstr = n.ToString ();
-		byte[] bytes = Encoding.ASCII.GetBytes (nstr);
-		sender.Send(bytes, nstr.Length);
+	void Update() {
+		// Send stuff
+		string nstr = "hello";
+		byte[] bytes = Encoding.ASCII.GetBytes(nstr);
+		int sent = sockOut.Send(bytes);
 
-		n++;
+		// Receive stuff
+		int nbytes = sockIn.Available;
+		if (nbytes > 0) {
+			byte[] data = new byte[4096];
+			sockIn.Receive(data);
+			Debug.Log(Encoding.ASCII.GetString(data));
+		}
 	}
 }
